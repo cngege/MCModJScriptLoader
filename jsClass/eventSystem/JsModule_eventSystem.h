29439b2b5c8b8f5204c4637b716d3c60f00e3b79
muzili <2586850402@qq.com>
2024-10-01 23:02:07 +0800

    添加事件系统模块

---
+++ jsClass/eventSystem/JsModule_eventSystem.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#ifndef JSCLASS_EVENTSYSTEM_JSMODULEEVENTSYSTEM_H
+#define JSCLASS_EVENTSYSTEM_JSMODULEEVENTSYSTEM_H
+
+#include <string>
+#include "quickjs/quickjs.h"
+#include <functional>
+
+// 二进制程序内部广播事件
+void NativeBroadcastEvent(const std::string& name, JSValueConst this_val = JS_UNDEFINED, int argc = 0, JSValueConst* argv = nullptr, std::function<void(bool)> callback = nullptr);
+// 二进制程序内部监听事件
+JSValue NativeListenEvent(const std::string& eventname, JSCFunction jsfun, const std::string& funname);
+
+
+#endif // !JSCLASS_EVENTSYSTEM_JSMODULEEVENTSYSTEM_H
