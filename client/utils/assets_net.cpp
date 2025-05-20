21e044ffb98f398f5572a60b5e9c34cfa4d8dcab
muzili <2586850402@qq.com>
2025-05-20 16:57:53 +0800

    下载字体不使用代理 + 新字体

--- client/utils/assets_net.cpp
+++ client/utils/assets_net.cpp
@@ -7,20 +7,33 @@
 #include <fstream>
 #include <iostream>
 #include <filesystem>
+#include "imgui/imgui.h"
 
 namespace fs = std::filesystem;
 
 void http::downFont_JNMYT(std::filesystem::path fontsDir) {
 	// httplib 下载字体文件
 	if(!fs::exists(fontsDir / "JNMYT.ttf")) {
-		httplib::Client fontdown("https://ghproxy.cc");
-		auto res = fontdown.Get("/https://github.com/cngege/MCModJScriptLoader/releases/download/0.0.1/JNMYT.ttf");
+		httplib::Client fontdown("https://github.com");
+		fontdown.enable_server_certificate_verification(false);
+		fontdown.set_follow_location(true);
+		auto res = fontdown.Get("/cngege/MCModJScriptLoader/releases/download/0.0.1/JNMYT1.2.ttf", [](uint64_t len, uint64_t total) {
+			spdlog::debug("{}% ==>  {} / {}", (int)(len * 100 / total), len, total);
+			return true;
+		});
 		if(res->status == httplib::StatusCode::OK_200) {
 			std::ofstream font(fontsDir / "JNMYT.ttf", std::ios::binary);
 			if(font.is_open()) {
 				font.write(res->body.data(), res->body.size());
 				font.close();
 				spdlog::info("字体文件已经下载: {}, size {}", "JNMYT.ttf", res->body.size());
+
+				// 装备至ImGui
+				ImGuiIO& io = ImGui::GetIO(); (void)io;
+				fs::path font_JNMYT = fontsDir / "JNMYT.ttf";
+				if(fs::exists(font_JNMYT)) {
+					io.Fonts->AddFontFromFileTTF(font_JNMYT.string().c_str(), 14.5f, NULL, io.Fonts->GetGlyphRangesChineseFull());
+				}
 			}
 			else {
 				spdlog::warn("字体文件已经下载完成，但在本地创建文件时失败");
