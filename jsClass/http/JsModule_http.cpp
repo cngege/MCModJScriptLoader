0fe1587d8650dc299af8872f4cb8c356c176c90a
muzili <2586850402@qq.com>
2025-06-04 09:24:39 +0800

    http模块调整优化

--- jsClass/http/JsModule_http.cpp
+++ jsClass/http/JsModule_http.cpp
@@ -3,6 +3,7 @@
 
 #define CPPHTTPLIB_OPENSSL_SUPPORT
 #include "http/httplib.h"
+#include "spdlog/spdlog.h"
 
 #include <string>
 
@@ -151,15 +152,20 @@ static JSValue js_http_ajax_request(JSContext* ctx, JSValueConst this_val, int a
         method = *hasMethod;
     }
 
+    // 拿到headers属性{}
+    JSValue JHeaders = JS_GetPropertyStr(ctx, argv[0], "headers");
+    std::optional<std::vector<std::pair<std::string, std::string>>> headers{};
+    headers = JSTool::getObjKV(JHeaders);
+
+
+    //拿到 success 属性回调函数
     JSValue JSuccessCall = JS_GetPropertyStr(ctx, argv[0], "success");
     bool hasSuccessCall = JS_IsFunction(ctx, JSuccessCall);
-
+    //拿到download属性回调函数
     JSValue JDownloadCall = JS_GetPropertyStr(ctx, argv[0], "download");
     bool hasDownloadCall = JS_IsFunction(ctx, JDownloadCall);
-    if(hasDownloadCall && method == "Post") {
-        return JS_ThrowTypeError(ctx, "Post请求不支持 Download 回调");
-    }
 
+    //followlocation属性bool值 301状态是否自动跳转
     JSValue Jfollowlocation = JS_GetPropertyStr(ctx, argv[0], "followlocation");
     JSValue Jtimeout = JS_GetPropertyStr(ctx, argv[0], "timeout");
     auto isTimeout = JSTool::toInt64(Jtimeout);
@@ -204,30 +210,73 @@ static JSValue js_http_ajax_request(JSContext* ctx, JSValueConst this_val, int a
         https.set_connection_timeout(isTimeout.value() * 1000);
     }
     
-    httplib::Result res;
-    if(method == "Post") {
-        res = https.Post(weibu);
+    httplib::Headers Clientheaders = {};
+    if(headers) {
+        for(auto& item : *headers) {
+            Clientheaders.emplace(item.first, item.second);
+        }
+    }
+    httplib::Params params = {};
+    // 拿到请求数据
+    JSValue Jbody = JS_GetPropertyStr(ctx, argv[0], "body");
+
+    std::string body;
+    if(JS_IsObject(Jbody)) {// 检查是否为对象
+        auto v = JS_JSONStringify(ctx, Jbody, JS_UNDEFINED, JS_UNDEFINED);
+        if(JS_IsException(v)) {
+            return v;
+        }
+        auto str = JSTool::toString(v);
+        if(!str) return JS_ThrowTypeError(ctx, "请求参数无法序列化为字符串");
+        body = *str;
     }
     else {
-        if(hasDownloadCall) {
-            res = https.Get(weibu, [&](uint64_t len, uint64_t total) {
-                JSValue argv2[] = {JS_NewInt64(ctx, (int64_t)len), JS_NewInt64(ctx, (int64_t)total)};
-                JSValue Jret = JS_Call(ctx, JDownloadCall, this_val, 2, argv2);
-                if(!JS_IsBool(Jret)) {
-                    return true;
-                }
-                else {
-                    return (bool)JS_ToBool(ctx, Jret);
-                }
-            });
-        }
-        else {
-            res = https.Get(weibu);
+        if(JS_IsString(Jbody)) {
+            auto str = JSTool::toString(Jbody);
+            if(!str) return JS_ThrowTypeError(ctx, "请求参数无法序列化为字符串");
+            body = *str;
         }
     }
+    // 如果有下载监听 则创建下载回调
+    httplib::Progress downloadcall = nullptr;
+    if(hasDownloadCall) {
+        downloadcall = [&](uint64_t len, uint64_t total) {
+            JSValue argv2[] = { JS_NewInt64(ctx, (int64_t)len), JS_NewInt64(ctx, (int64_t)total) };
+            JSValue Jret = JS_Call(ctx, JDownloadCall, this_val, 2, argv2);
+            if(!JS_IsBool(Jret)) {
+                return true;
+            }
+            else {
+                return (bool)JS_ToBool(ctx, Jret);
+            }
+        };
+    }
+    httplib::Result res;
+    if(method == "Post") {
+        std::string content_type = "application/json";
+        auto it = Clientheaders.find(std::string("content-type"));
+        if(it != Clientheaders.end()) {
+            content_type = it->second;
+        }
+        
+        res = https.Post(weibu, Clientheaders, body, content_type, downloadcall);
+    }
+    else {
+        
+        if(!params.empty()) {
+            //// 额外指定了参数
+            //auto wenhao = weibu.find("?");
+            //weibu += (wenhao == std::string::npos) ? "?" : "&";
+            //for(auto& item : *data) {
+            //    weibu += item.first;
+            //    weibu += "=";
+            //    weibu += item.second;
+            //}
+            spdlog::warn("Option.data 参数仅用于Post, Get请用?xx==xx格式");
+        }
+        res = https.Get(weibu, Clientheaders, downloadcall);
+    }
     
-
-
     if(!res) {
         if(hasSuccessCall) {
             return JS_UNDEFINED;
